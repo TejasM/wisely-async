691a7e28a88a5a862bdee91aabf83ef0b0e64502
Tejas Mehta <tejasmehta0@gmail.com>
2014-06-11 21:51:47 -0400

    Init

---
+++ server.js
@@ -0,0 +1,39 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs'),
+    qs = require('querystring')
+
+
+io.sockets.on('connection', function(socket) {
+    console.log('client connected');
+
+    socket.on('disconnect', function() {
+        console.log('client disconnected');
+    });
+
+});
+
+
+function handler(req, res) {
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += data;
+    });
+    req.on('end', function() {
+        var message = qs.parse(buffer);
+        console.log(message.event);
+        try {
+            data = JSON.parse(message.data);
+        } catch(e) {
+            data = message.data;
+        }
+        io.sockets.emit(message.event, data);
+        res.writeHead(200);
+        res.end('ok');
+    });
+}
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
